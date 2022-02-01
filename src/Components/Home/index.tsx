cbade832a1e3008bdadb0251e5f349b4d3a7f43c
Radium Rocket 30 <radiumrocket30@Radium-30.local>
2022-02-01 12:41:33 -0300

    add logout button

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,7 +1,32 @@
-import {Text} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import React from 'react';
-const Home = () => {
-  return <Text>Welcome to Radium Care</Text>;
+import CustomButton from '../shared/CustomButton';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../helpers/Types';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Welcome'>;
+
+const Home = ({navigation}: Props) => {
+  return (
+    <>
+      <Text style={styles.welcomeText}>Welcome to</Text>
+      <Text style={styles.radiumText}>RADIUM CARE</Text>
+      <CustomButton
+        onPress={() => navigation.navigate('Welcome')}
+        text="Logout"
+      />
+    </>
+  );
 };
+const styles = StyleSheet.create({
+  welcomeText: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  radiumText: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
 
 export default Home;
